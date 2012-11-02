b6eb01eaa7ecf659e3b2fd608794074959d6cb83
ryanfiorini <ryanwfiorini@gmail.com>
2012-11-02 15:27:04 -0400

    init repo

---
+++ AppMobiWP8JqMobiKitchenSinkTemplate/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using AppMobiWP8JqMobiKitchenSinkTemplate.Resources;
+
+namespace AppMobiWP8JqMobiKitchenSinkTemplate
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
