c5387f979be904e8455a9a1092631fa889a7dde2
ryanfiorini <ryanwfiorini@gmail.com>
2012-11-05 15:34:01 -0500

    keeping up

--- AppMobiWP8JqMobiKitchenSinkTemplate/html/plugins/jq.desktopBrowsers.js
+++ AppMobiWP8JqMobiKitchenSinkTemplate/html/plugins/jq.desktopBrowsers.js
@@ -83,6 +83,9 @@
 
 			mouseDown = true;
 			lastTarget = e.target;
+			if (e.target.nodeName.toLowerCase() == "a" && e.target.href.toLowerCase() == "javascript:;")
+			    e.target.href = "#";
+
 	        redirectMouseToTouch("touchstart", e);
 	        firstMove = true;
 	        cancelClickMove=false;
